bca236694d81eb0983d2340cd9e6a80b9228e588
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-02-27 19:33:06 +0400

    add 8

---
+++ script.js
@@ -0,0 +1,79 @@
+"use strict"; 
+
+// დავალება 1.
+
+// for (let x = 5; x < 100; x ++){
+//   console.log(x);
+// }
+
+//დავალება 2
+
+// let array1 = [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+// for (let i = 0; i < array1.length; i ++) {
+// if (array1[i] > 0 && array1[i] < 10){
+//   console.log(i);
+// }
+//    }
+
+// დავალება 3.
+
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < array2.length; i++) {
+//   if ((array2[i] = 5)) {
+//     console.log("aris");
+//   }
+//   break;
+// }
+
+// დავალება 4.
+
+// let array3 = [1, 2, 3, 4, 5];
+// let sum = 0
+// for (let x = 0; x < array3.length; x ++) {
+//   sum = sum + array3[x]
+
+// }
+// console.log(sum);
+
+// დავალება 5.
+
+// let array4 = [1, 2, 3, 7, 6, 9];
+// let sum = 0;
+// for (let x = 0; x < array4.length; x ++) {
+//   sum = sum + array4[x];
+//  }
+//  result = sum / array4.length
+//  console.log(result);
+
+// დავალება 6
+
+// let array5 = [1, 2, 3, 7, 6, 9];{
+//   for (let x = 0; x < 7; x ++)
+// console.log(x);
+// }
+
+// დავალება 7
+// let user = {
+//   firstname: "giorgi",
+//   lastname: "smith",
+//   age: 25,
+//   studentstatus: "active"
+// };
+// console.log(user.studentstatus);
+
+// დავალება 8
+
+// let user = {
+//   name: 'giorgi',
+//   age:  20,
+//   studentstatus: 'active',
+// }
+//  if (user.age < 18 && user.studentstatus == 'active'){
+//   console.log('hello');
+// } else if (user.name == 'levan'){
+//   console.log('hello levan');
+// } else if (user.studentstatus == 'active' || user.age < 25){
+//   console.log('hello world');}
+// else {
+//   console.log('error');
+// }
